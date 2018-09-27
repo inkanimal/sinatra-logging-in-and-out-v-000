33e7ff6f7e48f9a5b584291363317f771a91500b
geoffrey woodcock <theatomproject@gmail.com>
2018-09-27 01:00:37 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180809201339) do
+ActiveRecord::Schema.define(version: 20180809235029) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.float "balance"
+  end
 
 end
